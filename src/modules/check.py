5d5f3747967d06b7a7914edfd61f069e6b2d0040
Martín Basualdo <70729951+MartinBasualdo0@users.noreply.github.com>
2023-09-22 19:54:01 -0300

    checks with pretty rich prints

--- src/modules/check.py
+++ src/modules/check.py
@@ -1,19 +1,23 @@
 import os
+from rich import print
 
 # Function to check and create directories if needed
 def check_and_create_directories():
     if not os.path.exists("downloads"):
         os.mkdir("downloads")
-        print("Created 'downloads' folder.")
+        print("[bold green]Created 'downloads' folder.[/bold green]")
     if not os.path.exists("output"):
         os.mkdir("output")
-        print("Created 'output' folder.")
+        print("[bold green]Created 'output' folder.[/bold green]")
 
 # Function to check if api_key.py exists
 def check_api_key_file():
     if not os.path.exists("api_key.py"):
-        print("WARNING: 'api_key.py' file is missing. Please create it with your API key.\Feel free to read the README.md if you need help")
+        print("[bold red]WARNING: 'api_key.py' file is missing. Please create it with your API key.[/bold red]")
+        print("Feel free to read the README.md if you need help")
         exit(1)  # Exit the program with an error code
-    else: 
-        print("All checks passed")
-    
+    else:
+        print("[bold green]All checks passed[/bold green]")
+
+
+
